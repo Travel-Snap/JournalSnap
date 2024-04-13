aae1cf0e890400f6ea91ee34e1477a13c625207d
Abby Damodaran <abbydamodaran@Abbys-MacBook-Air.local>
2024-04-13 11:39:27 -0700

    SwiftUI project setup

---
+++ TravelSnap/TravelSnap/TravelSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelSnapApp.swift
+//  TravelSnap
+//
+//  Created by Abby Damodaran on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
