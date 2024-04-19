ea8f3acea3c9ed523a3b50879a74f4ca05cd3647
jinkimaz <143664962+jinkimaz@users.noreply.github.com>
2024-04-19 12:38:10 -0700

    home feed view

---
+++ TravelSnap/TravelSnap/Models/Post.swift
@@ -0,0 +1,46 @@
+// Post.swift
+import Foundation
+import FirebaseFirestoreSwift
+import SwiftUI
+
+struct Post: Identifiable, Codable {
+    @DocumentID var id: String?
+    var userID: String
+    var username: String
+    var userProfilePictureURL: URL
+    var photoURL: URL
+    var caption: String
+    var timestamp: Date
+    var likes: [String]
+    var comments: [Comment]
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userID
+        case username
+        case userProfilePictureURL
+        case photoURL
+        case caption
+        case timestamp
+        case likes
+        case comments
+    }
+}
+
+struct Comment: Identifiable, Codable {
+    @DocumentID var id: String?
+    var userID: String
+    var username: String
+    var userProfilePictureURL: URL
+    var text: String
+    var timestamp: Date
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userID
+        case username
+        case userProfilePictureURL
+        case text
+        case timestamp
+    }
+}
