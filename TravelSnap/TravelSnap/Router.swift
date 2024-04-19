39024d2d5497ff75d95e6072c3cdb09f72aefbd2
minaz94 <minakrn7@gmail.com>
2024-04-18 20:00:53 -0400

    Firebase + Authentication + Welcome + Router + Assets
    Firebase:
    - added package
    - integrated firebase
    
    Authentication:
    -sign up
    -sign in
    -log out
    
    Welcome:
    -check user's authentication
    
    Router:
    -added routing to the app
    
    Assets:
    -added assets for the authentication screen

---
+++ TravelSnap/TravelSnap/Router.swift
@@ -0,0 +1,37 @@
+//
+//  Router.swift
+//  TravelSnap
+//
+//  Created by Mina on 4/18/24.
+//
+
+import SwiftUI
+
+// Observable class responsible for managing navigation within the app
+@Observable
+class Router {
+    
+    // Enum defining possible destinations for navigation
+    enum Destination: Codable, Hashable {
+        case authentication
+        case test
+    }
+    
+    // NavigationPath to keep track of navigation history
+    var navigationPath = NavigationPath()
+    
+    // Function to navigate to a specific destination
+    func navigate(to destination: Destination) {
+        navigationPath.append(destination)
+    }
+    
+    // Function to dismiss the current destination and navigate back
+    func dismiss() {
+        navigationPath.removeLast()
+    }
+    
+    // Function to navigate back to the root view
+    func popToRoot() {
+        navigationPath.removeLast(navigationPath.count)
+    }
+}
