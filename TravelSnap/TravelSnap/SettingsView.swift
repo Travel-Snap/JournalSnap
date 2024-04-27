479bce2cd597e5c891e25c7dea34dd0d677283ca
jinkimaz <143664962+jinkimaz@users.noreply.github.com>
2024-04-26 23:02:33 -0700

    set profile image in SettingsView

--- TravelSnap/TravelSnap/SettingsView.swift
+++ TravelSnap/TravelSnap/SettingsView.swift
@@ -12,9 +12,38 @@ struct SettingsView: View {
     // Environment objects for accessing AuthViewModel and Router
     @Environment(AuthViewModel.self) var authViewModel
     @Environment(Router.self) var router
+    @ObservedObject var firebaseVM = FirebaseViewModel()
+    
+    @State private var showImagePicker = false
+    @State private var selectedImage: UIImage?
+    @State private var selectedItem: PhotosPickerItem?
+    
         
     var body: some View {
-
+        VStack {
+            Group {
+                // display user profile
+                if let profileImageURL = authViewModel.currentUser?.profilePictureURL {
+                    AsyncImage(url: URL(string: profileImageURL)) { image in
+                        image
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                    } placeholder: {
+                        ProgressView()
+                    }
+                } else {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding(.top)
+            .onTapGesture {
+                showImagePicker.toggle()
+            }
+            
             List {
                 Section {
                     // TODO: choose a profile picture - check func - same idea with the post
@@ -22,7 +51,7 @@ struct SettingsView: View {
                     NavigationLink(destination: PersonalInfoView()) {
                         Text("Personal information")
                         // TODO: change email - it's a button - check func
-                    
+                        
                     }
                     NavigationLink(destination: ChangePasswordView()) {
                         Text("Change password")
@@ -56,6 +85,21 @@ struct SettingsView: View {
                 }
             }
             .navigationBarTitle("Settings")
+        }
+        .sheet(isPresented: $showImagePicker) {
+                    ImagePicker(selectedImage: $selectedImage)
+                }
+        .onChange(of: selectedImage) { _ in
+            Task {
+                if let selectedImage = selectedImage {
+                    do {
+                        try await firebaseVM.setProfileImage(selectedImage: selectedImage)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -88,5 +132,6 @@ struct ChangeUsernameView: View {
     SettingsView()
         .environment(AuthViewModel())
         .environment(Router())
+        .environment(FirebaseViewModel())
     
 }
